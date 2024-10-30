d0ec00b599c61cbc76be7c8d3844992c1fb32cec
SalmaMj <115832927+SalmaMj@users.noreply.github.com>
2024-10-30 16:58:45 +0100

    Update KaddemApplicationTests.java

--- src/test/java/tn/esprit/kaddem/KaddemApplicationTests.java
+++ src/test/java/tn/esprit/kaddem/KaddemApplicationTests.java
@@ -1,16 +1,12 @@
 package tn.esprit.kaddem;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class KaddemApplicationTests {
-
-	@Test
-	void contextLoads() {
-		// Ajoutez une assertion pour vérifier que le contexte se charge correctement
-		assertTrue(true); // Changez ceci en l'assertion appropriée pour votre test
-	}
-
+@SpringBootApplication
+public class KaddemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KaddemApplication.class, args);
+    }
 }
+
